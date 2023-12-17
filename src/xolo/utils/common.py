b53d9ea6d6fcaa39b585ef6c79873803c3205abb
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-12-17 12:18:50 -0700

    common utils

--- src/xolo/utils/common.py
+++ src/xolo/utils/common.py
@@ -4,20 +4,20 @@ from xolo.utils.typing import H
 
 
 
-def is_dataclass(obj) -> bool:
+def is_dataclass_type(obj) -> bool:
     """
-    Determines whether the provided object is both a class and a dataclass.
+    Determines if the provided object is both a class type and a dataclass.
 
-    This function checks two conditions: whether 'obj' is a class type, 
-    and whether it is a dataclass. This is useful for ensuring that 'obj' 
-    not only adheres to the structure of a dataclass but is also a class definition 
-    rather than an instance of a class.
+    This function verifies whether the 'obj' is a class (i.e., a type object) and 
+    whether it is decorated with the dataclass decorator. This ensures that 'obj' 
+    is not just any class, but specifically a dataclass type, which is important 
+    for cases where behavior or attributes specific to dataclasses are required.
 
     Args:
-        obj: The object to be checked. It can be any Python object.
+        obj (Any): The object to be checked. It can be any Python object.
 
     Returns:
-        bool: True if 'obj' is a class and is a dataclass, False otherwise.
+        bool: True if 'obj' is a class type and is decorated as a dataclass, False otherwise.
     """
     return dataclasses.is_dataclass(obj) and isinstance(obj, type)
 
@@ -25,21 +25,72 @@ def is_dataclass(obj) -> bool:
 
 def is_dataclass_instance(obj) -> bool:
     """
-    Determines whether the given object is an instance of a dataclass.
+    Determines if the given object is an instance of a dataclass.
 
-    This function checks if 'obj' is an instance of a dataclass, as opposed to 
-    being the dataclass itself (i.e., the class definition). It does this by 
-    first checking if 'obj' is a dataclass and then ensuring that it is not 
-    a type (class definition). This is useful for differentiating between a 
-    dataclass and an instance of that dataclass.
+    This function checks if 'obj' is an instance of a class that is decorated as 
+    a dataclass. It differentiates between the class itself (the type object) and 
+    its instances. This is useful when needing to verify that 'obj' is an actual 
+    instantiated object of a dataclass, rather than just the dataclass definition.
 
     Args:
-        obj: The object to be checked. This can be any Python object.
+        obj (Any): The object to be checked. This can be any Python object.
 
     Returns:
         bool: True if 'obj' is an instance of a dataclass, False otherwise.
     """
     return dataclasses.is_dataclass(obj) and not isinstance(obj, type)
+
+
+
+def is_namedtuple_type(obj) -> bool:
+    """
+    Determines whether the provided object is a namedtuple type.
+
+    This function checks if 'obj' is a subclass of tuple and whether it has
+    characteristics specific to namedtuple types, such as the '_fields', '_make',
+    and '_asdict' attributes. This distinguishes namedtuple types from other 
+    tuple-like classes or instances.
+
+    Args:
+        obj (Any): The object to be checked. It can be any Python object.
+
+    Returns:
+        bool: True if 'obj' is a namedtuple type, False otherwise.
+    """
+    return (
+        isinstance(obj, type) and 
+        issubclass(obj, tuple) and 
+        hasattr(obj, '_fields') and 
+        hasattr(obj, '_make') and 
+        hasattr(obj, '_asdict')
+    )
+
+
+
+def is_namedtuple_instance(obj) -> bool:
+    """
+    Determines whether the provided object is an instance of a namedtuple.
+
+    This function checks if 'obj' is an instance of 'tuple' and possesses the '_fields' attribute, 
+    which is indicative of namedtuples. Furthermore, it verifies that '_fields' is a tuple 
+    consisting solely of strings and that 'obj' includes the '_asdict' method, a characteristic 
+    feature of namedtuples. These checks ensure that the object is not just any tuple, but 
+    specifically an instance of a namedtuple.
+
+    Args:
+        obj (Any): The object to be checked.
+
+    Returns:
+        bool: True if 'obj' is an instance of a namedtuple, False otherwise.
+    """
+    return (
+        isinstance(obj, tuple) and 
+        hasattr(obj, '_fields') and 
+        isinstance(obj._fields, tuple) and 
+        all(isinstance(field, str) for field in obj._fields) and
+        hasattr(obj, '_asdict') and 
+        callable(obj._asdict)
+    )
 
 
 
