6775c1300f9ad0eca8916a6bba7ab4cae2de6425
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-12-16 18:20:02 -0700

    dataclass

--- src/xolo/utils/common.py
+++ src/xolo/utils/common.py
@@ -1,5 +1,45 @@
+import dataclasses
 from collections.abc import Iterable
 from xolo.utils.typing import H
+
+
+
+def is_dataclass(obj) -> bool:
+    """
+    Determines whether the provided object is both a class and a dataclass.
+
+    This function checks two conditions: whether 'obj' is a class type, 
+    and whether it is a dataclass. This is useful for ensuring that 'obj' 
+    not only adheres to the structure of a dataclass but is also a class definition 
+    rather than an instance of a class.
+
+    Args:
+        obj: The object to be checked. It can be any Python object.
+
+    Returns:
+        bool: True if 'obj' is a class and is a dataclass, False otherwise.
+    """
+    return dataclasses.is_dataclass(obj) and isinstance(obj, type)
+
+
+
+def is_dataclass_instance(obj) -> bool:
+    """
+    Determines whether the given object is an instance of a dataclass.
+
+    This function checks if 'obj' is an instance of a dataclass, as opposed to 
+    being the dataclass itself (i.e., the class definition). It does this by 
+    first checking if 'obj' is a dataclass and then ensuring that it is not 
+    a type (class definition). This is useful for differentiating between a 
+    dataclass and an instance of that dataclass.
+
+    Args:
+        obj: The object to be checked. This can be any Python object.
+
+    Returns:
+        bool: True if 'obj' is an instance of a dataclass, False otherwise.
+    """
+    return dataclasses.is_dataclass(obj) and not isinstance(obj, type)
 
 
 
